a3bca5185e243906ad8d30419daaedc41846e2a2
Jeff MacDonald <jeff.macdonald@indexexchange.com>
2026-03-09 16:02:44 -0300

    nuke

--- app.js
+++
@@ -1,6 +0,0 @@
-// app.js
-const http = require('http');
-http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Hello World\n');
-}).listen(3000, () => console.log('Running on :3000'));
