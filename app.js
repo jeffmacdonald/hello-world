9da52057f638fbfa9b3ed81da6567b647196675d
Jeff MacDonald <jeff.macdonald@indexexchange.com>
2026-03-09 14:40:11 -0300

    initial

---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello World\n');
+}).listen(3000, () => console.log('Running on :3000'));
